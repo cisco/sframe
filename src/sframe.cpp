1ace20ca4c11c66d7822f2bef40f14ec2156989d
Richard Barnes <rlb@ipv.sx>
2020-09-13 14:39:06 -0400

    Move header encode/decode logic to its own files

--- src/sframe.cpp
+++ src/sframe.cpp
@@ -1,9 +1,7 @@
 #include <sframe/sframe.h>
 
 #include "crypto.h"
-
-#include <openssl/err.h>
-#include <openssl/evp.h>
+#include "header.h"
 
 #include <array>
 #include <iomanip>
@@ -69,31 +67,6 @@ Context::add_key(KeyID key_id, const bytes& base_key)
                          KeyState{ std::move(key), std::move(salt), 0 });
 }
 
-static size_t
-encode_uint(uint64_t val, output_bytes start)
-{
-  size_t size = 1;
-  while (val >> (8 * size) > 0) {
-    size += 1;
-  }
-
-  for (size_t i = 0; i < size; i++) {
-    start[size - i - 1] = uint8_t(val >> (8 * i));
-  }
-
-  return size;
-}
-
-static uint64_t
-decode_uint(input_bytes data)
-{
-  uint64_t val = 0;
-  for (size_t i = 0; i < data.size(); i++) {
-    val = (val << 8) + static_cast<uint64_t>(data[i]);
-  }
-  return val;
-}
-
 static bytes
 form_nonce(CipherSuite suite, Counter ctr, const bytes& salt)
 {
@@ -110,63 +83,6 @@ form_nonce(CipherSuite suite, Counter ctr, const bytes& salt)
   return nonce;
 }
 
-static constexpr size_t min_header_size = 1;
-static constexpr size_t max_header_size = 1 + 8 + 8;
-
-static size_t
-encode_header(KeyID kid, Counter ctr, output_bytes data)
-{
-  size_t kid_size = 0;
-  if (kid > 0x07) {
-    kid_size = encode_uint(kid, data.subspan(1));
-  }
-
-  size_t ctr_size = encode_uint(ctr, data.subspan(1 + kid_size));
-  if ((ctr_size > 0x07) || (kid_size > 0x07)) {
-    throw std::runtime_error("Header overflow");
-  }
-
-  data[0] = uint8_t(ctr_size << 4);
-  if (kid <= 0x07) {
-    data[0] |= kid;
-  } else {
-    data[0] |= 0x08 | kid_size;
-  }
-
-  return 1 + kid_size + ctr_size;
-}
-
-static std::tuple<KeyID, Counter, input_bytes>
-decode_header(input_bytes data)
-{
-  if (data.size() < min_header_size) {
-    throw std::runtime_error("Ciphertext too small to decode header");
-  }
-
-  auto cfg = data[0];
-  auto ctr_size = size_t((cfg >> 4) & 0x07);
-  auto kid_long = (cfg & 0x08) > 0;
-  auto kid_size = size_t(cfg & 0x07);
-
-  auto kid = KeyID(kid_size);
-  if (kid_long) {
-    if (data.size() < 1 + kid_size) {
-      throw std::runtime_error("Ciphertext too small to decode KID");
-    }
-
-    kid = KeyID(decode_uint(data.subspan(1, kid_size)));
-  } else {
-    kid_size = 0;
-  }
-
-  if (data.size() < 1 + kid_size + ctr_size) {
-    throw std::runtime_error("Ciphertext too small to decode CTR");
-  }
-  auto ctr = Counter(decode_uint(data.subspan(1 + kid_size, ctr_size)));
-
-  return std::make_tuple(kid, ctr, data.subspan(0, 1 + kid_size + ctr_size));
-}
-
 output_bytes
 Context::protect(KeyID key_id, output_bytes ciphertext, input_bytes plaintext)
 {
@@ -179,11 +95,7 @@ Context::protect(KeyID key_id, output_bytes ciphertext, input_bytes plaintext)
   const auto ctr = st.counter;
   st.counter += 1;
 
-  if (ciphertext.size() < max_header_size) {
-    throw std::runtime_error("Ciphertext to small to encod header");
-  }
-
-  auto hdr_size = encode_header(key_id, ctr, ciphertext);
+  auto hdr_size = Header{key_id, ctr}.encode(ciphertext);
   auto header = ciphertext.subspan(0, hdr_size);
   auto inner_ciphertext = ciphertext.subspan(hdr_size);
 
@@ -196,17 +108,17 @@ Context::protect(KeyID key_id, output_bytes ciphertext, input_bytes plaintext)
 output_bytes
 Context::unprotect(output_bytes plaintext, input_bytes ciphertext)
 {
-  auto [kid, ctr, header] = decode_header(ciphertext);
-  auto inner_ciphertext = ciphertext.subspan(header.size());
+  auto [header, aad] = Header::decode(ciphertext);
+  auto inner_ciphertext = ciphertext.subspan(aad.size());
 
-  auto it = state.find(kid);
+  auto it = state.find(header.key_id);
   if (it == state.end()) {
     throw std::runtime_error("Unknown key");
   }
 
   const auto& st = it->second;
-  const auto nonce = form_nonce(suite, ctr, st.salt);
-  return open(suite, st.key, nonce, plaintext, header, inner_ciphertext);
+  const auto nonce = form_nonce(suite, header.counter, st.salt);
+  return open(suite, st.key, nonce, plaintext, aad, inner_ciphertext);
 }
 
 } // namespace sframe
