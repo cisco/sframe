da3681bfa6d9f52afb7af8fda94b7a9380deece5
Pascal Bühler <pabuhler@cisco.com>
2020-10-14 16:38:10 +0200

    use default destructor
    Co-authored-by: Richard Barnes <rlb@ipv.sx>

--- src/sframe.cpp
+++ src/sframe.cpp
@@ -92,8 +92,7 @@ SFrame::SFrame(CipherSuite suite_in)
   : suite(suite_in)
 {}
 
-SFrame::~SFrame()
-{}
+SFrame::~SFrame() = default;
 
 output_bytes
 SFrame::_protect(KeyID key_id, output_bytes ciphertext, input_bytes plaintext)
