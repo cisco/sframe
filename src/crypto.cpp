022e754c693cac5e644c390d90dbdfa16c0a7121
Richard Barnes <rlb@ipv.sx>
2021-02-19 16:10:59 -0500

    Remove all the printfs but the one that seems to matter

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -138,7 +138,7 @@ HMAC::HMAC(CipherSuite suite, input_bytes key)
   auto type = openssl_digest_type(suite);
   auto key_size = static_cast<int>(key.size());
   if (1 != HMAC_Init_ex(ctx.get(), key.data(), key_size, type, nullptr)) {
-    printf("HMAC::HMAC: %p %p %d %p %d\n", (void*)ctx.get(), (void*)key.data(), key_size, (void*)type, EVP_MD_type(type));
+    //printf("HMAC::HMAC: %p %p %d %p %d\n", (void*)ctx.get(), (void*)key.data(), key_size, (void*)type, EVP_MD_type(type));
     throw openssl_error("HMAC init");
   }
 }
@@ -167,7 +167,7 @@ HMAC::digest()
 bytes
 hkdf_extract(CipherSuite suite, const bytes& salt, const bytes& ikm)
 {
-  printf("hkdf_extract: %d %p %lu\n", (uint16_t)suite, (void*)salt.data(), (unsigned long)salt.size());
+  //printf("hkdf_extract: %d %p %lu\n", (uint16_t)suite, (void*)salt.data(), (unsigned long)salt.size());
   auto hmac = HMAC(suite, salt);
   hmac.write(ikm);
   auto mac = hmac.digest();
@@ -193,7 +193,7 @@ hkdf_expand(CipherSuite suite,
 
   auto label = info;
   label.push_back(0x01);
-  printf("hkdf_expand: %d %p %lu\n", (uint16_t)suite, (void*)secret.data(), (unsigned long)secret.size());
+  //printf("hkdf_expand: %d %p %lu\n", (uint16_t)suite, (void*)secret.data(), (unsigned long)secret.size());
   auto hmac = HMAC(suite, secret);
   hmac.write(label);
   auto mac = hmac.digest();
@@ -268,7 +268,7 @@ seal_ctr(CipherSuite suite,
   ctr_crypt(suite, enc_key, nonce, inner_ct, pt);
 
   // Authenticate with truncated HMAC
-  printf("seal_ctr: %d %p %lu\n", (uint16_t)suite, (void*)auth_key.data(), (unsigned long)auth_key.size());
+  //printf("seal_ctr: %d %p %lu\n", (uint16_t)suite, (void*)auth_key.data(), (unsigned long)auth_key.size());
   auto hmac = HMAC(suite, auth_key);
   hmac.write(aad);
   hmac.write(inner_ct);
@@ -384,7 +384,7 @@ open_ctr(CipherSuite suite,
   auto auth_key = key_span.subspan(enc_key_size);
 
   // Authenticate with truncated HMAC
-  printf("open_ctr: %d %p %lu\n", (uint16_t)suite, (void*)auth_key.data(), (unsigned long)auth_key.size());
+  //printf("open_ctr: %d %p %lu\n", (uint16_t)suite, (void*)auth_key.data(), (unsigned long)auth_key.size());
   auto hmac = HMAC(suite, auth_key);
   hmac.write(aad);
   hmac.write(inner_ct);
