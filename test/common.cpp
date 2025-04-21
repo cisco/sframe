3be8857515b5821cb06a073ff457c043443a4ed6
Richard Barnes <rlb@ipv.sx>
2025-04-21 17:54:09 -0400

    Add missing files

---
+++ test/common.cpp
@@ -0,0 +1,32 @@
+#include "common.h"
+
+#include <iomanip>
+#include <sstream>
+
+bytes
+from_hex(const std::string& hex)
+{
+  if (hex.length() % 2 == 1) {
+    throw std::invalid_argument("Odd-length hex string");
+  }
+
+  auto len = int(hex.length() / 2);
+  auto out = bytes(len);
+  for (int i = 0; i < len; i += 1) {
+    auto byte = hex.substr(2 * i, 2);
+    out[i] = static_cast<uint8_t>(strtol(byte.c_str(), nullptr, 16));
+  }
+
+  return out;
+}
+
+std::string
+to_hex(const sframe::input_bytes data)
+{
+  std::stringstream hex(std::ios_base::out);
+  hex.flags(std::ios::hex);
+  for (const auto& byte : data) {
+    hex << std::setw(2) << std::setfill('0') << int(byte);
+  }
+  return hex.str();
+}
---
+++ test/common.h
@@ -0,0 +1,18 @@
+#include <sframe/sframe.h>
+
+#include <string>
+#include <vector>
+
+using bytes = std::vector<uint8_t>;
+
+bytes
+from_hex(const std::string& hex);
+std::string
+to_hex(const sframe::input_bytes data);
+
+template<typename T>
+bytes
+to_bytes(const T& range)
+{
+  return bytes(range.begin(), range.end());
+}
